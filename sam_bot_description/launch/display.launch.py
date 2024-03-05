4625b8eaab1e5b472a635ce89f5acac084e2a60f
Tony <tonychan1488@yahoo.com.hk>
2024-03-05 23:01:44 +0800

    Odometry before Build,Run and Verification

--- sam_bot_description/launch/display.launch.py
+++ sam_bot_description/launch/display.launch.py
@@ -18,14 +18,9 @@ def generate_launch_description():
         executable='joint_state_publisher',
         name='joint_state_publisher',
         arguments=[default_model_path],
-        condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
+        
     )
-    joint_state_publisher_gui_node = launch_ros.actions.Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
-    )
+    
     rviz_node = launch_ros.actions.Node(
         package='rviz2',
         executable='rviz2',
@@ -33,16 +28,24 @@ def generate_launch_description():
         output='screen',
         arguments=['-d', LaunchConfiguration('rvizconfig')],
     )
+    
+ spawn_entity = launch_ros.actions.Node(
+  package='gazebo_ros',
+  executable='spawn_entity.py',
+  arguments=['-entity', 'sam_bot', '-topic', 'robot_description'],
+  output='screen'
+)
 
     return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
-                                            description='Flag to enable joint_state_publisher_gui'),
+        launch.actions.,
         launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
                                             description='Absolute path to robot urdf file'),
         launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
                                             description='Absolute path to rviz config file'),
+        launch.actions.ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'], output='screen'),
         joint_state_publisher_node,
         joint_state_publisher_gui_node,
         robot_state_publisher_node,
+        spawn_entity,
         rviz_node
     ])
